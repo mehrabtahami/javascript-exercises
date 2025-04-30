4336fc6dbc4ca2bf285e02ac53ef27f41c0a9436
Younes <mehrabtahami@gmail.com>
2025-04-30 14:26:46 +0330

    passed 3 tests of 02 practice - trying to pass the 4th

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let output = string;
+  let n = num;
+  for (let i = 1; i < n; i++) {
+    output += string;
+  }
+  return output;
 };
 
 // Do not edit below this line
