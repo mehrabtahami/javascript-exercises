6dff533c01a25bf561be053601edecbe2c93931b
Younes <mehrabtahami@gmail.com>
2025-05-02 14:37:43 +0330

    Solved with include method

--- 04_removeFromArray/README.md
+++ 04_removeFromArray/README.md
@@ -1,3 +1,5 @@
+Done✅
+
 # Exercise 04 - removeFromArray
 
 Implement a function that takes an array and some other arguments then removes the other arguments from that array:
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+  const newArr = [];
+  arr.forEach((item) => {
+    if (!nums.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
 };
 
 // Do not edit below this line
