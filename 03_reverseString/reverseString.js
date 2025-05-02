2b1eb79f036d96c0aa6fe04fc0a75ea79f1440f9
Younes <mehrabtahami@gmail.com>
2025-05-02 12:57:55 +0330

    Solved & Passed All 4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let input = str.split("");
 
+  let output = "";
+  // below line is taking string length and minus 1 from it(for example 5-1 = 4) & then return it
+  // then at the end it will return string length 0 (first charachter "i >= 0" for that last character)
+  // works from the end of string to start of the starting (negative loop)
+  for (let i = input.length - 1; i >= 0; i--) {
+    output += input[i];
+  }
+  return output;
 };
 
 // Do not edit below this line
