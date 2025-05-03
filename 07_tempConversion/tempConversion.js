6e94c99189218c3f3057774c0da4e9455d7bcac3
Younes <mehrabtahami@gmail.com>
2025-05-03 15:11:19 +0330

    solved with formula and math round function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function () {
+  let fTc = arguments[0];
+  let result = ((fTc - 32) * 5) / 9;
+  let output = Math.round(result * 10) / 10;
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function () {
+  let cTf = arguments[0];
+  let result = (cTf * 9) / 5 + 32;
+  let output = Math.round(result * 10) / 10;
+  return output;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
