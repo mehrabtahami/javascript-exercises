f2f69eb0182ea86db8ea38b62f06aef9fd824858
Younes <mehrabtahami@gmail.com>
2025-05-03 14:26:16 +0330

    Solved with multiple nested ifs

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
-
+const leapYears = function () {
+  const year = arguments[0];
+  let output;
+  let four = year % 4;
+  let oneHun = year % 100;
+  let fourHun = year & 400;
+  if (four === 0) {
+    if (oneHun === 0) {
+      if (fourHun === 0) {
+        output = true;
+      } else {
+        output = false;
+      }
+    } else {
+      output = true;
+    }
+  } else {
+    output = false;
+  }
+  return output;
 };
 
 // Do not edit below this line
