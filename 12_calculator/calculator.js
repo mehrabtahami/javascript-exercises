432eec64f4ac18cd81dfb2a240c0c5a1fd0ba599
Younes <mehrabtahami@gmail.com>
2025-05-15 15:06:30 +0330

    solved 12 - caclulator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+  return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+    return arguments[0].reduce((acc,item) => {acc += item
+    return acc;
+    },0);
 };
 
 const multiply = function() {
-
+    return arguments[0].reduce((acc,item) => {acc *= item
+    return acc;
+    },1);
 };
 
 const power = function() {
-	
+	let number = arguments[0];
+    let power = arguments[1];
+    let output = 1;
+    for (let i = 0 ; i < power ; i++){
+       output *= number ;
+    }
+    return output;
 };
 
 const factorial = function() {
-	
+  let number = arguments[0];
+  let output = 1;
+  for (let i = number ; i >= 1 ; i--){
+    output *= i ;
+  }
+  return output;
 };
 
 // Do not edit below this line
