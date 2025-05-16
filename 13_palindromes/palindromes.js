93919721ffab6b8f8e8871522c60c3e5aa1d8714
Younes <mehrabtahami@gmail.com>
2025-05-16 13:25:20 +0330

    Solved the 13th palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
 const palindromes = function () {
+  let specialChar = [" ", "/", "@", "!", ".", ",", "#", "$", "%"];
+  let inputSplit = arguments[0].toLowerCase().split("");
+  let converted = inputSplit.filter((item) => !specialChar.includes(item));
 
+  console.log(`ORG : ${inputSplit}`);
+  console.log(`CONVERTED : ${converted} => REVERSED : ${converted.reverse()}`);
+
+  if (converted.join("") === converted.reverse().join("")) {
+    console.log("true");
+    return true;
+  } else {
+    console.log("false");
+    return false;
+  }
 };
 
 // Do not edit below this line
