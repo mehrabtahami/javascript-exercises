ad0a75a4155d153106707222b54b874186677d96
Younes <mehrabtahami@gmail.com>
2025-05-16 14:16:01 +0330

    solved the 16 find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function () {
+  const input = arguments[0];
+  let currentYear = 2025;
+  const ages = input.sort((a, b) => {
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = currentYear;
+    }
+    if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = currentYear;
+    }
+    if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return ages[0];
 };
 
 // Do not edit below this line
