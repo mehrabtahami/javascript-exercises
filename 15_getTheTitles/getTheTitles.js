3f963ccedac0008c47a5f089b247ce5e4bc14798
Younes <mehrabtahami@gmail.com>
2025-05-16 13:47:22 +0330

    solved the 15th get The titles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function () {
+  const titles = arguments[0];
+  return titles.map((item) => item.title);
 };
 
 // Do not edit below this line
