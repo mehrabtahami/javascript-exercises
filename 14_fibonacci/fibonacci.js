6dddcfb7ced3938cfa26b6eeb55280da94293fcd
Younes <mehrabtahami@gmail.com>
2025-05-16 13:43:35 +0330

    sovled the Fibonacci for 14th practice

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function () {
+  const number = arguments[0];
+  if (number < 0) {
+    return "OOPS";
+  } else {
+    const fib = [0, 1];
+    for (let i = 2; i <= number; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[number];
+  }
 };
 
 // Do not edit below this line
